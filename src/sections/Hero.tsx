796463130836519593f0fff33a483a9b2cb31435
Hamed <ostovari.eng@gmail.com>
2025-07-22 20:19:16 +0200

    Added video to Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,16 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
+
+  const isTablet = useMediaQuery({
+    query: "(max-width: 1024px)",
+  });
   useGSAP(() => {
     const splitTitle = SplitText.create(".hero-title", {
       type: "chars",
@@ -13,6 +21,7 @@ const Hero = () => {
       duration: 1,
       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       ease: "circ.out",
+      delay: 0.2,
     });
 
     tl.from(
@@ -64,11 +73,28 @@ const Hero = () => {
   return (
     <section className="bg-main-bg hero-section overflow-hidden">
       <div className="hero-container">
-        <img
-          src="/images/hero-img.png"
-          alt="hero image"
-          className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
-        />
+        {isTablet ? (
+          <>
+            {isMobile && (
+              <img
+                src="/images/hero-bg.png"
+                className="absolute bottom-40 size-full object-cover"
+              />
+            )}
+            <img
+              src="/images/hero-img.png"
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto"
+            />
+          </>
+        ) : (
+          <video
+            src="/videos/hero-bg.mp4"
+            autoPlay
+            muted
+            playsInline
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        )}
         <div className="hero-content">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious</h1>
